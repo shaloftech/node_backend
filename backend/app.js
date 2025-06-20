5a71d60519a4a420b30f0ee71003d36f6768499e
shaloftech <shaloftech@gmail.com>
2025-06-20 12:25:52 +0545

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -39,11 +39,11 @@ app.use("/api/v1", payment);
 
 const __dirname1 = path.resolve();
 
-app.use(express.static(path.join(__dirname1, "/frotend/build")));
+// app.use(express.static(path.join(__dirname1, "/frotend/build")));
 
-app.get("*", (req, res) =>
-  res.sendFile(path.resolve(__dirname1, "frotend", "build", "index.html"))
-);
+// app.get("*", (req, res) =>
+//   res.sendFile(path.resolve(__dirname1, "frotend", "build", "index.html"))
+// );
 
 
 module.exports = app;
